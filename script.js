4f46648c453d2e2ad6fb1d0637cc00440c94f547
jmatzko <145479293+jmatzko@users.noreply.github.com>
2024-01-26 21:24:31 -0500

    figured out way to create grid now working on developing a slider that can manipulate grid

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ const sketchBox = document.querySelector('.sketch-container');
 
 
 
+function makeGrid() {
+    
+    for(let i = 0; i < 16; i++) {
+        let row = document.createElement('div')
+        row.classList.add("row" + i);
+        
+        sketchBox.appendChild(row);
+        let rowW = document.querySelector('.row' + i);
 
+            for(let j = 0; j < 16; j++) {
+                let column = document.createElement('div');
+                column.classList.add('column');
+                rowW.appendChild(column);
+            }
+    }
+}
+
+
+makeGrid();
