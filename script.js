d7e44861f92d16c30c22f7f2943306ed1edcdf43
jmatzko <145479293+jmatzko@users.noreply.github.com>
2024-01-28 12:55:27 -0500

    fully working yet to add buttons

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
-const sketchBox = document.querySelector('.sketch-container');
+const sketchBox = document.querySelector('.sketch');
+const sliderEl = document.querySelector('#slide-value');
+const sliderValue = document.querySelector('.value');
+
+
+let gridSize = 16;
 
 
 
-function makeGrid() {
+sliderEl.addEventListener("input", (event) => {
     
-    for(let i = 0; i < 16; i++) {
-        let row = document.createElement('div')
-        row.classList.add("row" + i);
-        
-        sketchBox.appendChild(row);
-        let rowW = document.querySelector('.row' + i);
+    const tempSliderValue = event.target.value;
+    sliderValue.textContent = tempSliderValue + "x" + tempSliderValue;
 
-            for(let j = 0; j < 16; j++) {
-                let column = document.createElement('div');
-                column.classList.add('column');
-                rowW.appendChild(column);
-            }
+    const progress = ((tempSliderValue - sliderEl.min) / (sliderEl.max - sliderEl.min)) * 100;
+    sliderEl.style.background = `linear-gradient(to right, #ff0 ${progress}%, #ccc ${progress}%)`;
+        
+});
+
+
+sliderEl.addEventListener("input", function(e) {
+    gridSize = e.target.value;
+    clearGrid();
+    makeGrid(gridSize);   
+        
+});
+
+
+function clearGrid() {
+    while (sketchBox.firstChild) {
+        sketchBox.removeChild(sketchBox.firstChild);
     }
 }
 
+function changeColor() {
+    let color = "color: black";
+    return color;
+}
 
-makeGrid();
+function makeGrid(gridSize) {
+    
+    
+    for(let i = 0; i < gridSize; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.classList.add('row');
+        sketchBox.appendChild(gridBox);
+
+            for(let j = 0; j < gridSize; j++) {
+                const gridColumn = document.createElement('div');
+                gridColumn.classList.add('column');
+                gridBox.appendChild(gridColumn);
+            }
+    }
+    
+    const gridColor = document.querySelectorAll('.column');
+    gridColor.forEach(function(gridColor) {
+        gridColor.addEventListener("mouseover", () => {
+            gridColor.style.background = "black";
+    });
+});
+}
+
+
+
